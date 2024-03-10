2084306f1d2623a38d2948234ef3505d2ffacf25
Sergio Díaz Cancillo <uo287627@uniovi.es>
2024-03-10 18:01:00 +0100

    Borrado "console.log()"

--- webapp/src/components/Game.js
+++ webapp/src/components/Game.js
@@ -66,7 +66,6 @@ const Game = ({username}) => {
         const resultCorrecta = data.results.bindings[indexCorrecta];
         setInformacionWikidata(resultCorrecta[questionLabel].value + '?');
         setRespuestaCorrecta(resultCorrecta[answerLabel].value);
-        //console.log("Obtener datos: answerCorrect: " + respuestaCorrecta);
 
         // Obtener respuestas falsas
         const respuestas = [];
@@ -126,14 +125,11 @@ const Game = ({username}) => {
 
   const generarBotonesRespuestas = useCallback(async () => {
     try{
-      console.log("Generando botones");
       const correctPos = Math.floor(Math.random() * 4) + 1;
-      console.log(correctPos);
       const buttonsData = [];
       let contWrongAnsw = 0;
       for(let i=1; i<=4; i++){
         if(i===correctPos){
-          console.log("Generando boton correcta: "+respuestaCorrecta);
           buttonsData.push({ answer: respuestaCorrecta, handler: handleButtonClickCorrect });
         }else{
           buttonsData.push({ answer: respuestasFalsas[contWrongAnsw], handler: handleButtonClickGeneric });
@@ -148,8 +144,6 @@ const Game = ({username}) => {
   }, [respuestaCorrecta, respuestasFalsas, handleButtonClickCorrect, handleButtonClickGeneric]);
 
   useEffect(() => {
-    console.log("Bien: "+respuestaCorrecta);
-    console.log("Mal: "+respuestasFalsas);
     generarBotonesRespuestas();
   }, [respuestaCorrecta, respuestasFalsas, generarBotonesRespuestas]);
 
