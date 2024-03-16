eb01351e47161127bc2ca0d2119f4fe6596cd565
uo283055 <uo283055@uniovi.es>
2024-03-16 01:10:44 +0100

    proceso de mover a micro las queries

--- questions/createservice/create-service.js
+++ questions/createservice/create-service.js
@@ -1,60 +1,118 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 const Question = require('./create-model');
 
 const app = express();
 const port = 8005;
 
-// Middleware to parse JSON in request body
+// Middleware para analizar JSON en el cuerpo de la solicitud
 app.use(bodyParser.json());
 
 // Connect to MongoDB
 const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://aswuser:aswuser@wiq06b.hsfgpcm.mongodb.net/questiondb?retryWrites=true&w=majority&appName=wiq06b';
 mongoose.connect(mongoUri);
 
+// Tipos de preguntas y consultas a Wikidata
+const questionTypes = {
+  pais: {
+    query: `
+      SELECT ?country ?countryLabel ?capital ?capitalLabel
+      WHERE {
+        ?country wdt:P31 wd:Q6256.
+        ?country wdt:P36 ?capital.
+        SERVICE wikibase:label {
+          bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es".
+        }
+      }
+      ORDER BY RAND()
+      LIMIT 150
+    `,
+    questionLabel: 'countryLabel',
+    answerLabel: 'capitalLabel'
+  },
+  // Agregar otros tipos de preguntas aquí
+};
 
-  // Route for user login
+// Ruta para agregar una nueva pregunta
 app.post('/addQuestion', async (req, res) => {
   try {
-    const newQuestion1 = new Question({
+    const newQuestion = new Question({
       questionBody: req.body.questionBody,
       typeQuestion: req.body.typeQuestion,
       typeAnswer: req.body.typeAnswer,
-  });
-  newQuestion1.save();
-  res.json(newQuestion1);
-  
+    });
+    await newQuestion.save();
+    res.json(newQuestion);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-
-//obtiene una pregunta de forma aleatoria
+// Ruta para obtener una pregunta de forma aleatoria
 app.post('/getQuestionBody', async (req, res) => {
   try {
-    
-    //modelo mongo
-    const Question = mongoose.model('Question');
-    //saco una pregunta de forma aleatoria
     const rQuestion = await Question.aggregate([{ $sample: { size: 1 } }]);
-    
     res.json(rQuestion[0]);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-// Start the server
-const server = app.listen(port, () => {
-  console.log(`Auth Service listening at http://localhost:${port}`);
+// Nuevo endpoint para obtener una pregunta completa
+app.get('/getFullQuestion', async (req, res) => {
+  try {
+    const rQuestion = await Question.aggregate([{ $sample: { size: 1 } }]);
+    const questionType = rQuestion[0].typeQuestion;
+    const { query, questionLabel, answerLabel } = questionTypes[questionType];
+
+    const apiUrl = `https://query.wikidata.org/sparql?query=${encodeURIComponent(query)}`;
+    const headers = { "Accept": "application/json" };
+    const respuestaWikidata = await fetch(apiUrl, { headers });
+
+    if (respuestaWikidata.ok) {
+      const data = await respuestaWikidata.json();
+      const numEles = data.results.bindings.length;
+
+      const indexCorrecta = Math.floor(Math.random() * numEles);
+      const resultCorrecta = data.results.bindings[indexCorrecta];
+      const informacionWikidata = resultCorrecta[questionLabel].value + '?';
+      const respuestaCorrecta = resultCorrecta[answerLabel].value;
+
+      const respuestasFalsas = [];
+      for (let i = 0; i < 3; i++) {
+        const indexFalsa = Math.floor(Math.random() * numEles);
+        const resultFalsa = data.results.bindings[indexFalsa];
+        respuestasFalsas.push(resultFalsa[answerLabel].value);
+      }
+
+      const body=rQuestion[0].questionBody+informacionWikidata;
+
+      const fullQuestion = {
+        questionBody: body,
+        correctAnswer: respuestaCorrecta,
+        incorrectAnswers: respuestasFalsas
+      };
+
+      res.json(fullQuestion);
+    } else {
+      res.status(500).json({ error: "Error al realizar la consulta en Wikidata. Estado de respuesta:" + respuestaWikidata.status });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error al realizar la consulta en Wikidata" });
+  }
 });
 
-server.on('close', () => {
-    // Close the Mongoose connection
-    mongoose.connection.close();
-  });
+// Iniciar el servidor
+const server = app.listen(port, () => {
+  console.log(`Servicio de autenticación escuchando en http://localhost:${port}`);
+});
 
-module.exports = server
-  
+// Manejar el cierre del servidor
+server.on('close', () => {
+  // Cerrar la conexión de Mongoose
+  mongoose.connection.close();
+});
+
+module.exports = server;
