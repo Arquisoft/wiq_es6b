e4735bd9f219e112711a76d7842ac0ea2b8341ab
uo283055 <uo283055@uniovi.es>
2024-04-01 21:39:56 +0200

    pruebas tst generatedQuestion

--- webapp/src/components/GeneratedQuestionsList.test.js
+++ webapp/src/components/GeneratedQuestionsList.test.js
@@ -1,40 +1,27 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import GeneratedQuestionsList from '../components/GeneratedQuestionsList';
 import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import GeneratedQuestionsList from './GeneratedQuestionsList';
 
-jest.mock('axios');
+const mockAxios = new MockAdapter(axios);
 
 describe('GeneratedQuestionsList component', () => {
-  test('should render list of generated questions', async () => {
-    // Define el mock de axios para simular una respuesta exitosa
-    const mockedQuestions = [
-      { generatedQuestionBody: 'Question 1', correctAnswer: 'Answer 1' },
-      { generatedQuestionBody: 'Question 2', correctAnswer: 'Answer 2' }
-    ];
-    axios.get.mockResolvedValueOnce({ data: mockedQuestions });
-
-    render(<GeneratedQuestionsList />);
-
-    // Espera a que las preguntas se muestren en la lista
-    await waitFor(() => {
-      mockedQuestions.forEach(question => {
-        expect(screen.getByText(question.generatedQuestionBody)).toBeInTheDocument();
-        expect(screen.getByText(question.correctAnswer)).toBeInTheDocument();
-      });
-    });
+  beforeEach(() => {
+    mockAxios.reset();
   });
 
-  test('should handle error when fetching questions', async () => {
-    // Define el mock de axios para simular una respuesta con error 500
-    axios.get.mockRejectedValueOnce({ response: { status: 500 } });
-
+  it('should display "Lista de preguntas" header', async () => {
     render(<GeneratedQuestionsList />);
 
-    // Espera a que el mensaje de error se muestre en la pantalla
-    await waitFor(() => {
-      expect(screen.getByText('Error obteniendo la lista de preguntas generadas')).toBeInTheDocument();
-    });
+    const header = screen.getByText(/Lista de preguntas/i);
+    expect(header).toBeInTheDocument();
+  });
+
+  it('should display the table', async () => {
+    render(<GeneratedQuestionsList />);
+
+    const table = screen.getByRole('table');
+    expect(table).toBeInTheDocument();
   });
 });
