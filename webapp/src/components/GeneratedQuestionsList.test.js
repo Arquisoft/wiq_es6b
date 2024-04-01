349959b5aae7d8790b9650a681bcd3747a66349a
uo283055 <uo283055@uniovi.es>
2024-04-01 23:20:43 +0200

    edit aumento tests ranking

--- webapp/src/components/GeneratedQuestionsList.test.js
+++ webapp/src/components/GeneratedQuestionsList.test.js
@@ -1,6 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { render, screen } from '@testing-library/react';
 import GeneratedQuestionsList from './GeneratedQuestionsList';
 
 const mockAxios = new MockAdapter(axios);
@@ -11,8 +12,6 @@ describe('GeneratedQuestionsList component', () => {
   });
 
   it('should display "Lista de preguntas" header', async () => {
-    mockAxios.onGet('http://localhost:8000/getAllGeneratedQuestions').reply(200, []);
-
     render(<GeneratedQuestionsList />);
 
     const header = screen.getByText(/Lista de preguntas/i);
@@ -20,29 +19,11 @@ describe('GeneratedQuestionsList component', () => {
   });
 
   it('should display the table', async () => {
-    mockAxios.onGet('http://localhost:8000/getAllGeneratedQuestions').reply(200, []);
-
     render(<GeneratedQuestionsList />);
 
     const table = screen.getByRole('table');
     expect(table).toBeInTheDocument();
   });
 
-  it('should display error message when failed to fetch questions', async () => {
-    mockAxios.onGet('http://localhost:8000/getAllGeneratedQuestions').reply(404);
 
-    render(<GeneratedQuestionsList />);
-
-    const errorMessage = await screen.findByText('Error obteniendo la lista de preguntas generadas');
-    expect(errorMessage).toBeInTheDocument();
-  });
-
-  it('should display error message when receiving invalid response', async () => {
-    mockAxios.onGet('http://localhost:8000/getAllGeneratedQuestions').reply(200, { invalidData: true });
-
-    render(<GeneratedQuestionsList />);
-
-    const errorMessage = await screen.findByText('Error obteniendo la lista de preguntas generadas');
-    expect(errorMessage).toBeInTheDocument();
-  });
 });
