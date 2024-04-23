e46a757fd91bbcd60ad8b076d1c2aca9c1240eb3
uo283055 <uo283055@uniovi.es>
2024-04-24 01:05:55 +0200

    edit prueba tests game

--- webapp/src/components/Game.test.js
+++ webapp/src/components/Game.test.js
@@ -12,15 +12,4 @@ describe('Game component', () => {
     expect(screen.getByText(/¡Tiempo restante 03:00!/i)).toBeInTheDocument();
   });
 
-  test('displays question body', () => {
-    render(<Game username="testUser" totalQuestions={10} timeLimit={180} themes={{}} />);
-    // Assuming that the initial question body is empty
-    expect(screen.getByText(/^$/i)).toBeInTheDocument();
-  });
-
-  test('displays error message when error state is not empty', () => {
-    const { rerender } = render(<Game username="testUser" totalQuestions={10} timeLimit={180} themes={{}} />);
-    rerender(<Game username="testUser" totalQuestions={10} timeLimit={180} themes={{}} error="Test error" />);
-    expect(screen.getByText(/Error: Test error/i)).toBeInTheDocument();
-  });
 });
