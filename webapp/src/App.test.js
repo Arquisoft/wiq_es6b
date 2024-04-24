cc51cd186c9ba4c59e9ad9ac3366c3e3eb95d119
uo283055 <uo283055@uniovi.es>
2024-04-24 18:16:16 +0200

    aumento tests app

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login view by default', () => {
   render(<App />);
-  //const linkElement = screen.getByText(/Welcome to wiq_6B/i);
-  //expect(linkElement).toBeInTheDocument();
+  const loginElement = screen.getByText(/Inicia sesión aqui./i);
+  expect(loginElement).toBeInTheDocument();
+});
+
+test('renders register view when register link is clicked', () => {
+  render(<App />);
+  const registerLink = screen.getByRole('button', { name: /¿No tienes cuenta? Registrate aqui./i });
+  registerLink.click();
+  const registerElement = screen.getByText(/¿Ya tienes cuenta? Inicia sesión aqui./i);
+  expect(registerElement).toBeInTheDocument();
+});
+
+test('renders login view when login link is clicked in register view', () => {
+  render(<App />);
+  const registerLink = screen.getByRole('button', { name: /¿No tienes cuenta? Registrate aqui./i });
+  registerLink.click();
+  const loginLink = screen.getByRole('button', { name: /¿Ya tienes cuenta? Inicia sesión aqui./i });
+  loginLink.click();
+  const loginElement = screen.getByText(/Inicia sesión aqui./i);
+  expect(loginElement).toBeInTheDocument();
 });
