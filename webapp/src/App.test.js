ccb369c64a0af10ad53a80bd999f0c56ce7a2415
uo283055 <uo283055@uniovi.es>
2024-04-24 18:31:20 +0200

    aumento tests app

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -3,23 +3,23 @@ import App from './App';
 
 test('renders login view by default', () => {
   render(<App />);
-  const loginElement = screen.getByText(/Inicia sesión aqui./i);
+  const loginElement = screen.getByText(/Iniciar sesión/i);
   expect(loginElement).toBeInTheDocument();
 });
 
-test('renders register view when register link is clicked', () => {
+test('renders register view when register link is clicked', async () => {
   render(<App />);
-  const registerLink = screen.getByRole('button', { name: /¿No tienes cuenta? Registrate aqui./i });
+  const registerLink = await screen.findByRole('button', { name: /¿No tienes cuenta? Registrate aqui./i });
   registerLink.click();
   const registerElement = screen.getByText(/¿Ya tienes cuenta? Inicia sesión aqui./i);
   expect(registerElement).toBeInTheDocument();
 });
 
-test('renders login view when login link is clicked in register view', () => {
+test('renders login view when login link is clicked in register view', async () => {
   render(<App />);
-  const registerLink = screen.getByRole('button', { name: /¿No tienes cuenta? Registrate aqui./i });
+  const registerLink = await screen.findByRole('button', { name: /¿No tienes cuenta? Registrate aqui./i });
   registerLink.click();
-  const loginLink = screen.getByRole('button', { name: /¿Ya tienes cuenta? Inicia sesión aqui./i });
+  const loginLink = await screen.findByRole('button', { name: /¿Ya tienes cuenta? Inicia sesión aqui./i });
   loginLink.click();
   const loginElement = screen.getByText(/Inicia sesión aqui./i);
   expect(loginElement).toBeInTheDocument();
