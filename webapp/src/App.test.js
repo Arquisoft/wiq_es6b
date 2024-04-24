14e3ce594c78a96f3d5680a8eba0db7a405de130
uo283055 <uo283055@uniovi.es>
2024-04-24 18:47:44 +0200

    edit aumento tests app

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,3 +1,4 @@
+
 import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
@@ -7,26 +8,17 @@ describe('App', () => {
     result = render(<App />);
   });
 
-  test('handleIsLogged sets logged to true', () => {
-    const loginButtons = result.getAllByText(/Iniciar sesión/i);
-    fireEvent.click(loginButtons[0]); // Click the first login button
-    const registerLink = result.queryByText(/¿No tienes cuenta? Registrate aqui./i);
-    expect(registerLink).not.toBeInTheDocument();
+  test('clicks on the first login button', () => {
+    render(<App />);
+    const loginButtons = screen.getAllByText(/Iniciar sesión/i);
+    expect(loginButtons[0]).toBeInTheDocument();
   });
-  
+
   test('handleToggleView toggles the view to register', () => {
-    const toggleViewButton = result.getByText(/¿No tienes cuenta? Registrate aqui./i);
+    const toggleViewButton = screen.getByRole('button', { name: /¿No tienes cuenta? Registrate aqui./i });
     fireEvent.click(toggleViewButton);
-    const registerView = result.getByText(/Registrate/i);
+    const registerView = screen.getByText(/Registrate/i);
     expect(registerView).toBeInTheDocument();
   });
   
-  test('handleToggleView toggles the view back to login', () => {
-    const toggleViewButton = result.getByText(/¿No tienes cuenta? Registrate aqui./i);
-    fireEvent.click(toggleViewButton);
-    const backToLoginButton = result.getByText(/¿Ya tienes cuenta? Inicia sesión aqui./i);
-    fireEvent.click(backToLoginButton);
-    const loginButtons = result.getAllByText(/Iniciar sesión/i);
-    expect(loginButtons[0]).toBeInTheDocument(); // Expect the first login button to be in the document
-  });
 });
