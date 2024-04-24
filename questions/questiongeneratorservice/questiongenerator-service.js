5ef7bbe072110669845481e0c7830038c4576246
uo277310 <uo277310@uniovi.es>
2024-04-25 00:11:38 +0200

    Arreglo de direccion

--- questions/questiongeneratorservice/questiongenerator-service.js
+++ questions/questiongeneratorservice/questiongenerator-service.js
@@ -18,9 +18,8 @@ const port = 8007;
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-const mongoUser = process.env.MONGO_USER;
 const mongoPassword = process.env.MONGO_PASSWORD;
-const mongoUri = `mongodb+srv://${mongoUser}:${mongoPassword}@wiq06b.hsfgpcm.mongodb.net/questiongeneratordb?retryWrites=true&w=majority&appName=wiq06b`;
+const mongoUri = `mongodb+srv://aswuser:${mongoPassword}@wiq06b.hsfgpcm.mongodb.net/questiongeneratordb?retryWrites=true&w=majority&appName=wiq06b`;
 mongoose.connect(mongoUri);
 
 
