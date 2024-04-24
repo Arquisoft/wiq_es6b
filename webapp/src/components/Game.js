155d01d0c095e25d03d6663ba74b83d8ca72adaa
uo277310 <uo277310@uniovi.es>
2024-04-24 20:17:43 +0200

    Cambiado uso de random

--- webapp/src/components/Game.js
+++ webapp/src/components/Game.js
@@ -17,11 +17,33 @@ const Game = ({ username, totalQuestions, timeLimit, themes }) => {
     const pricePerQuestion = 25;
     const delayBeforeNextQuestion = 3000; // 3 segundos de retardo antes de pasar a la siguiente pregunta
 
-    const crypto = window.crypto || window.msCrypto;
-    const array = new Uint32Array(1);
-    crypto.getRandomValues(array);
-
     const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
+
+    function getRandomIndex(length) {
+        let randomIndex;
+        if (typeof window !== 'undefined' && window.crypto) {
+          const crypto = window.crypto || window.msCrypto;
+          const array = new Uint32Array(1);
+          crypto.getRandomValues(array);
+          randomIndex = array[0] % length;
+        } else {
+          // Usar una alternativa que funcione en Node.js
+          randomIndex = Math.floor(Math.random() * length);
+        }
+        return randomIndex;
+    } 
+
+    function randomSort() {
+        if (typeof window !== 'undefined' && window.crypto) {
+          const crypto = window.crypto || window.msCrypto;
+          const array = new Uint32Array(1);
+          crypto.getRandomValues(array);
+          return array[0] / 4294967295 > 0.5 ? 1 : -1;
+        } else {
+          // Usar una alternativa que funcione en Node.js
+          return Math.random() > 0.5 ? 1 : -1;
+        }
+    }
 
     if(isNaN(totalQuestions)){
         totalQuestions=10;
@@ -49,13 +71,13 @@ const Game = ({ username, totalQuestions, timeLimit, themes }) => {
             try {
                 const temas = Object.entries(themesSelected).filter(([tema, seleccionado]) => seleccionado)
                                                             .map(([tema]) => tema);
-                const randomIndex = array[0] % temas.length;
+                const randomIndex = getRandomIndex(temas.length);
                 const temaAleatorio = temas[randomIndex];
                 
                 const response = await axios.get(`${apiEndpoint}/getRandomQuestion${temaAleatorio}`);
                 setQuestion(response.data);
                 const respuestas = [...response.data.incorrectas, response.data.correcta];
-                setRespuestasAleatorias(respuestas.sort(() => (array[0] / 4294967295) - 0.5).slice(0, 4)); // Mostrar solo 4 respuestas
+                setRespuestasAleatorias(respuestas.sort(randomSort).slice(0, 4)); // Mostrar solo 4 respuestas
             } catch (error) {
                 console.error("Error al obtener la pregunta aleatoria", error);
                 setError('Error al obtener la pregunta aleatoria');
@@ -97,13 +119,13 @@ const Game = ({ username, totalQuestions, timeLimit, themes }) => {
         try {
             const temas = Object.entries(themesSelected).filter(([tema, seleccionado]) => seleccionado)
                                                             .map(([tema]) => tema);
-            const randomIndex = array[0] % temas.length;
+            const randomIndex = getRandomIndex(temas.length);
             const temaAleatorio = temas[randomIndex];
             
             const response = await axios.get(`${apiEndpoint}/getRandomQuestion${temaAleatorio}`);
             setQuestion(response.data);
             const respuestas = [...response.data.incorrectas, response.data.correcta];
-            setRespuestasAleatorias(respuestas.sort(() => (array[0] / 4294967295) - 0.5).slice(0, 4)); // Mostrar solo 4 respuestas
+            setRespuestasAleatorias(respuestas.sort(randomSort).slice(0, 4)); // Mostrar solo 4 respuestas
         } catch (error) {
             console.error("Error al obtener la pregunta aleatoria", error);
             setError('Error al obtener la pregunta aleatoria');
