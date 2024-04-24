305f8effede20692b18d67021e730bfeb0d3548f
uo283055 <uo283055@uniovi.es>
2024-04-24 19:43:28 +0200

    prueba aumento 2 tests app

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -12,18 +12,12 @@ describe('App', () => {
     const loginButtons = screen.getAllByText(/Iniciar sesión/i);
     expect(loginButtons[0]).toBeInTheDocument();
   });
-
-  test('handles login', () => {
-    const { getByText } = result;
-    fireEvent.click(getByText("Iniciar sesión", { selector: 'button' }));
-   
-  });
   
 
   test('toggles view between login and register', () => {
     const { getByText } = result;
-    fireEvent.click(getByText(/¿No tienes cuenta? Registrate aqui./i));
+    fireEvent.click(getByText("¿Ya tienes cuenta? Inicia sesión aqui."));
     const loginButtons = screen.getAllByText(/Añadir usuario/i);
-    expect(loginButtons).toBeInTheDocument();
+    expect(loginButtons[0]).toBeInTheDocument();
   });
 });
