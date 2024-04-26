12b93d2064432b6fed48c2da2db6e47bc0591177
uo285427 <uo285427@uniovi.es>
2024-04-26 02:46:28 +0200

    Primera version adaptabilidad para moviles

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import AddUser from './components/AddUser';
 import Login from './components/Login';
-//import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 function App() {
   const [showLogin, setShowLogin] = useState(true);
@@ -20,32 +20,30 @@ function App() {
   };
 
   return (
-    <Container component="main" maxWidth="lg" style={{ marginTop: '2rem', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-
-  <Paper elevation={3} style={{ padding: '2rem', textAlign: 'center', height: '550px', width: '1200px', overflow: 'auto' }}>
-
-      {showLogin ? <Login setLogged={handleIsLogged}/> : <AddUser />}
-        {!isLogged ? (<Typography component="div" align="center" sx={{ marginTop: 4 }}>
-            {showLogin ? (
-
-
-
-
-                <Link name="gotoregister" component="button" variant="body2" onClick={handleToggleView}>
-                    ¿No tienes cuenta? Registrate aqui.
-                </Link>
-            ) : (
-                <Link component="button" variant="body2" onClick={handleToggleView}>
-                    ¿Ya tienes cuenta? Inicia sesión aqui.
-                </Link>
+    <Container component="main" maxWidth="lg" style={{ marginTop: '2rem' }}>
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item xs={12} sm={8} md={6} lg={4}>
+          <Paper elevation={3} style={{ padding: '2rem', textAlign: 'center', overflow: 'auto' }}>
+            {showLogin ? <Login setLogged={handleIsLogged}/> : <AddUser />}
+            {!isLogged && (
+              <Typography component="div" align="center" sx={{ marginTop: 4 }}>
+                {showLogin ? (
+                  <Link name="gotoregister" component="button" variant="body2" onClick={handleToggleView}>
+                    ¿No tienes cuenta? Registrate aquí.
+                  </Link>
+                ) : (
+                  <Link component="button" variant="body2" onClick={handleToggleView}>
+                    ¿Ya tienes cuenta? Inicia sesión aquí.
+                  </Link>
+                )}
+              </Typography>
             )}
-        </Typography>
-        ) : (
-            <></>
-        ) }
-    </Paper>
+          </Paper>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
 
 export default App;
+
