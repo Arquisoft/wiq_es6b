df271481d73fa0ffeaf7b8577aa6db523b73e83a
Sergio Díaz Cancillo <uo287627@uniovi.es>
2024-04-28 18:55:51 +0200

    Subiendo coverage del nuevo código

--- webapp/src/components/GeneratedQuestionsList.test.js
+++ webapp/src/components/GeneratedQuestionsList.test.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import GeneratedQuestionsList from './GeneratedQuestionsList';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+jest.mock('axios');
+
+const mockAxios = new MockAdapter(axios);
 
 describe('GeneratedQuestionsList component', () => {
-  // Test for rendering the component and checking the main heading
+  beforeEach(() => {
+    axios.get.mockResolvedValue({
+      status: 200,
+      data: [
+        {
+          generatedQuestionBody: "¿A qué género literario pertenece 'Cinco horas con Mario'?",
+          correcta: 'Narrativo'
+        },
+        {
+          generatedQuestionBody: "¿De qué grupo es la canción 'Vino Tinto'?",
+          correcta: 'Estopa'
+        },
+        {
+          generatedQuestionBody: "¿Cuál es la capital de Portugal?",
+          correcta: 'Lisboa'
+        },
+        {
+          generatedQuestionBody: "¿Quién escribió la novela 'El Extranjero'?",
+          correcta: 'Albert Camus'
+        },
+      ],
+    });
+  });
+
   test('renders GeneratedQuestionsList component and main heading', () => {
     render(<GeneratedQuestionsList setError={() => {}} />);
 
-    // Check if the main heading is in the document
     const heading = screen.getByRole('heading', { name: /Lista de preguntas/i });
     expect(heading).toBeInTheDocument();
   });
 
-  // Test for rendering the table
   it('should display the table', () => {
     render(<GeneratedQuestionsList setError={() => {}} />);
 
@@ -20,24 +47,74 @@ describe('GeneratedQuestionsList component', () => {
     expect(table).toBeInTheDocument();
   });
 
-  // Test for rendering the table headers
   test('renders table headers', () => {
     render(<GeneratedQuestionsList setError={() => {}} />);
 
-    // Check if the table headers are in the document
     const questionHeader = screen.getByRole('columnheader', { name: /Pregunta/i });
     const answerHeader = screen.getByRole('columnheader', { name: /Respuesta Correcta/i });
     expect(questionHeader).toBeInTheDocument();
     expect(answerHeader).toBeInTheDocument();
   });
 
-  // Test for rendering the table rows
-test('renders table rows', () => {
-  render(<GeneratedQuestionsList setError={() => {}} />);
+  test('renders table rows', () => {
+    render(<GeneratedQuestionsList setError={() => {}} />);
 
-  // Check if the table rows are in the document
-  const tableRows = screen.getAllByRole('row');
-  expect(tableRows).not.toHaveLength(0);
-});
+    const tableRows = screen.getAllByRole('row');
+    expect(tableRows).not.toHaveLength(0);
+  });
+
+  test('should order questions by questionBody correctly', async () => {
+    const questionBodyHeader = screen.getByRole('columnheader', { name: /Pregunta/i });
+          
+    await act(async() => {
+      questionBodyHeader.click();
+    });
+
+    let rows = await screen.findAllByRole('row');
+
+    expect(rows[1]).toHaveTextContent('¿A qué género literario pertenece "Cinco horas con Mario"?');
+    expect(rows[2]).toHaveTextContent('¿Cuál es la capital de Portugal?');
+    expect(rows[3]).toHaveTextContent('¿De qué grupo es la canción "Vino Tinto"?');
+    expect(rows[4]).toHaveTextContent('¿Quién escribió la novela "El Extranjero"?');
+
+
+    await act(async() => {
+      questionBodyHeader.click();
+    });
+
+    rows = await screen.findAllByRole('row');
+
+    expect(rows[4]).toHaveTextContent('¿A qué género literario pertenece "Cinco horas con Mario"?');
+    expect(rows[3]).toHaveTextContent('¿Cuál es la capital de Portugal?');
+    expect(rows[2]).toHaveTextContent('¿De qué grupo es la canción "Vino Tinto"?');
+    expect(rows[1]).toHaveTextContent('¿Quién escribió la novela "El Extranjero"?');
+  });
+
+  test('should order questions by answer correctly', async () => {
+    const answerHeader = screen.getByRole('columnheader', { name: /Respuesta Correcta/i });
+          
+    await act(async() => {
+      answerHeader.click();
+    });
+
+    let rows = await screen.findAllByRole('row');
+
+    expect(rows[1]).toHaveTextContent('Albert Camus');
+    expect(rows[2]).toHaveTextContent('Estopa');
+    expect(rows[3]).toHaveTextContent('Lisboa');
+    expect(rows[4]).toHaveTextContent('Narrativo');
+
+
+    await act(async() => {
+      answerHeader.click();
+    });
+
+    rows = await screen.findAllByRole('row');
+
+    expect(rows[4]).toHaveTextContent('Albert Camus');
+    expect(rows[3]).toHaveTextContent('Estopa');
+    expect(rows[2]).toHaveTextContent('Lisboa');
+    expect(rows[1]).toHaveTextContent('Narrativo');
+  });
 
 });
