8f570127b714a616f110e68555c2c2af602005cf
Sergio Díaz Cancillo <uo287627@uniovi.es>
2024-04-28 11:46:19 +0200

    Corrigiendo fallo retorno mockAxios en Login Test

--- webapp/src/components/Login.test.js
+++ webapp/src/components/Login.test.js
@@ -36,12 +36,9 @@ describe('Login Component', () => {
     expect(usernameInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
 
-    expect(screen.getByText(/Probando una cosa/i)).toBeInTheDocument();
-
-
     // Mock the axios.post & axios.get requests to simulate successful responses
     mockAxios.onPost('http://localhost:8000/login').reply(200, { createdAt: '2024-01-01T12:34:56Z' });
-    mockAxios.onGet('http://localhost:8000/getAllUsers').reply(200);
+    mockAxios.onGet('http://localhost:8000/getAllUsers').reply(200, []);
     mockAxios.onPost('http://localhost:8000/createUserRank').reply(200);
 
     await act(async () => {
