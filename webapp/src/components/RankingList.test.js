27336cb9adc422ac381c2793b9004ebeaa23c847
uo283055 <uo283055@uniovi.es>
2024-04-30 00:30:49 +0200

    issues sonar

--- webapp/src/components/RankingList.test.js
+++ webapp/src/components/RankingList.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor, act } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import RankingList from './RankingList';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
