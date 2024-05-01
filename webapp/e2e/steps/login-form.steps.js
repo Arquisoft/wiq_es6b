442182da5b94f50d9841d34b4e42c13a0fa44b62
uo285427 <uo285427@uniovi.es>
2024-05-01 07:28:00 +0200

    arreglos

--- webapp/e2e/steps/login-form.steps.js
+++ webapp/e2e/steps/login-form.steps.js
@@ -39,6 +39,7 @@ defineFeature(feature, test => {
     });
 
     when('I fill the data in the form and press submit', async () => {
+        await expect(page).toClick("button", { text: "¿Ya tienes cuenta? Inicia sesión aqui." });
         await expect(page).toFill('input[name="username"]', username);
         await expect(page).toFill('input[name="password"]', password);
         await expect(page).toClick('button', { text: 'Iniciar sesión' })
