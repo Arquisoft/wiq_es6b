c3753f675da7808847fdd01aff0c8062f4105e21
uo277310 <uo277310@uniovi.es>
2024-05-01 17:22:03 +0200

    Arreglo cors

--- gatewayservice/gateway-service.js
+++ gatewayservice/gateway-service.js
@@ -27,9 +27,20 @@ const prodUrl3000 = process.env.PROD_URL_3000;
 const prodUrl8000 = process.env.PROD_URL_8000;
 
 const corsOptions = {
-  origin: '*',
+  origin: [devUrl3000, devUrl8000, prodUrl3000, prodUrl8000],
   optionsSuccessStatus: 200
 }
+
+const allowedOrigins = [devUrl3000, devUrl8000, prodUrl3000, prodUrl8000];
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin,Content-Type,Authorization');
+  next();
+});
 
 app.use(cors(corsOptions));
 app.use(express.json());
